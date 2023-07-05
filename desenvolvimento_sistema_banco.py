1a0f5b74176c54bdeeb13a9251411c5184286fe9
Pedro&Dev <pvcg2003@gmail.com>
2023-07-05 11:37:20 -0300

    Update desenvolvimento_sistema_banco.py

--- desenvolvimento_sistema_banco.py
+++ desenvolvimento_sistema_banco.py
@@ -7,7 +7,15 @@ menu="""
 [q] para sair
 ----------------------------------------
 """
-
+regras_de_uso = """
+Regras do banco:
+--------------------------------------------------------------------------
+O valor máximo de saque é de R$500
+Só podem ser realizados um máximo de 3 saques por dia 
+Caso não haja saldo em sua conta a operação de saque ficará indisponível
+--------------------------------------------------------------------------
+Agradecemos a compreensão
+"""
 
 saldo = 0
 limite = 500
@@ -17,7 +25,8 @@ LIMITE_SAQUE=3
 
 while True:
     opcao = input(menu)
-
+    print(regras_de_uso)
+    
     if opcao == "d":
         valor = float(input("Informe o valor a ser depositado"))
         if valor > 0:
@@ -29,6 +38,18 @@ while True:
     elif opcao == "s":
         valor=float(input("Informe o valor a ser sacado"))
         
+        teste_limite = valor > limite
+        teste_saldo = valor > saldo
+        teste_saque = numero_saques > LIMITE_SAQUE
+        
+        situacao_aprovado = teste_limite and teste_saldo and teste_saque
+
+        if situacao_aprovado and valor > 0:
+            saldo -= valor
+            extrato += f"Saque: R$ {valor:.2f}\n"
+            numero_saques += 1
+        else:
+            print("Operação falhou verifique os dados informados e tente novamente")    
 
     elif opcao == "e":
         print("\n***************** EXTRATO *****************")
